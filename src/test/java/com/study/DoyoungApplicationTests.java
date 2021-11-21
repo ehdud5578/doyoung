071dbc0d12d1a1c0bcbba96e27fe5d270c1871a6
parkdoyoung <ehdud5578@gmail.com>
2021-11-21 15:55:53 +0900

    first commit

---
+++ src/test/java/com/study/DoyoungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoyoungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

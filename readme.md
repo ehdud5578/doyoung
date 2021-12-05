fe06c1194c712d48a26627ce650b09edc349f449
parkdoyoung <ehdud5578@gmail.com>
2021-12-05 22:45:32 +0900

    add : first unit test

--- readme.md
+++ readme.md
@@ -1,1 +1,18 @@
-initial commit
+# DoYoung 
+
+` 직접 crud 게시판을 만들며 직접 테스트코드를 작성하고, 제가 격은 문제들과 어떻게 해결했는지를 기록해 나가겠습니다.
+나중에 이 프로젝트가 완성되면 reamdme.md 는 수정될 예정입니다.`
+
+
+
+### 프로젝트에 사용할 기술
+ - spring boot
+ - jpa
+ - mysql
+ - Thymeleaf
+
+
+#### Thymeleaf
+ - 기본 루트 : resources - templates
+
+
---
+++ src/test/java/com/study/controller/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.study.controller;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("dsunni")));
+
+    }
+}
